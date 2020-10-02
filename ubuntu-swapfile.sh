64ae5b52e5b53b1db208702f8e489c5e87e6886a
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-01 18:27:51 -0600

    updated commandline install echos

--- ubuntu-swapfile.sh
+++ ubuntu-swapfile.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 #SWAPFILE
-echo "Would you like to create a 2G swapfile? [Y/n]"
+echo "starting swapfile install script ..."
+echo "would you like to create a 2G swapfile? [Y/n]"
 read SWAP_CONFIRM
 if [ "$SWAP_CONFIRM" != "${SWAP_CONFIRM#[Yy]}" ] ;then
     fallocate -l 2G /swapfile
     chmod 600 /swapfile
     mkswap /swapfile
     swapon /swapfile
-    echo "# Linux Swapfile use sudo free -h to view available space" >> /etc/fstab
+    echo "updating /etc/fstab file to run swap file on system startup ..."
+    echo "# linux swapfile use sudo free -h to view available space" >> /etc/fstab
     echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
     echo "Success: swapfile created ..."
+    
 else
     echo "Notice: swapfile skipped by user ..."
 fi
